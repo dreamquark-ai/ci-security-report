89a3eb9f0394b6ea5506ab11675a71ec8438359a
paulb <barrie_p@etna-alternance.net>
2021-05-28 11:02:43 +0200

    feat: add security report action

---
+++ main.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -e
+FOLDER=$(readlink -f "${BASH_SOURCE[0]}" | xargs dirname)
+. "${FOLDER}/parse-json.sh"
+. "${FOLDER}/md-template.sh"
+. "${FOLDER}/comment-pr.sh"
+
+
+base_report="$FOLDER/reports/report-base.json"
+new_report="$FOLDER/reports/report-new.json"
+target_folder="$FOLDER/reports"
+target="$FOLDER/reports/security.md"
+report_folder="${FOLDER}/reports"
+
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -i|--image)
+    image="$2"
+    shift 
+    shift 
+    ;;
+    -bt|--base-tag)
+    base_tag="$2"
+    shift 
+    shift 
+    ;;
+    -nt|--new-tag)
+    new_tag="$2"
+    shift 
+    shift 
+    ;;
+    -r|--repo)
+    repo="$2"
+    shift 
+    shift 
+    ;;
+    -pr|--pull-request)
+    pr="$2"
+    shift 
+    shift 
+    ;;
+    -t|--topic)
+    topic="$2"
+    shift 
+    shift 
+    ;;
+    *)    
+    POSITIONAL+=("$1")
+    shift 
+    ;;
+esac
+done
+
+echo "Compare the differences between $base_report and $new_report."
+get_differences $base_report $new_report $target_folder 
+echo "Generate the report for $image:$base_tag->$new_tag."
+generate_markdown $report_folder $target $image $base_tag $new_tag $topic
+echo "Publish the report as a comment of the PR #$pr to $repo related to $topic"
+comment_pr $repo $pr $topic
